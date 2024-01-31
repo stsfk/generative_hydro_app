e9cf46a0db13f505ec6b80aff456c1cfa04ffedd
stsfk <yyang90@connect.hku.hk>
2024-01-31 23:17:35 +0800

    update

--- app_calibration.py
+++ app_calibration.py
@@ -14,6 +14,12 @@ import HydroErr
 
 import pygad
 
+# write title texts
+st.subheader("Search the parameter space to identify optimal model instances -- hydrological model calibration.")
+st.markdown(
+    "*[Optional] Upload climate forcing and discharge time series data for calibration and test periods. If no data are uploaded, data of Fish River near Fort Kent, Maine, US (USGS gauge ID: 01013500) will be used. The optimization problem is solved using the genetic algorithm (GA).*"
+)
+
 # load model
 decoder = torch.load(
     "data/final_lstm_decoder_test.pt", map_location=torch.device("cpu")
@@ -22,7 +28,7 @@ decoder = torch.load(
 decoder.eval()
 
 
-# Input calibration time series 
+# Input calibration time series
 st.sidebar.markdown(
     "## Upload a *calibration* climate forcing and discharge time series data [Optional]."
 )
@@ -32,7 +38,7 @@ uploaded_file_calibration = st.sidebar.file_uploader(
 )
 
 
-# Input test time series 
+# Input test time series
 st.sidebar.markdown(
     "## Upload a *test* climate forcing and discharge time series data [Optional]."
 )
@@ -73,29 +79,63 @@ warm_up = st.sidebar.number_input(
 )
 
 
+# input number of generaion
+st.sidebar.markdown("## Select the number of generations of GA.")
+
+num_generations = st.sidebar.number_input(
+    "Input the number of generations.",
+    min_value=2,
+    max_value=500,
+    value=30,
+)
+
+# input sol_per_pop
+st.sidebar.markdown("## Select the population size of GA.")
+
+sol_per_pop = st.sidebar.number_input(
+    "Input the population size.",
+    min_value=20,
+    max_value=500,
+    value=40,
+)
+
+
+# input sol_per_pop
+st.sidebar.markdown("## Select the number of solutions to be selected as parents..")
+
+num_parents_mating = st.sidebar.number_input(
+    "Input the number of solutions to be selected as parents.",
+    min_value=2,
+    max_value=sol_per_pop,
+    value=10,
+)
+
+
 class Objective_builder:
     def __init__(self, x, y):
         self.x = x.contiguous()
         self.y = y.contiguous()
-    
+
     def eval(self, ga_instance, solution, solution_idx):
-        
         # numpy to torch tensor
         solution = torch.from_numpy(solution).unsqueeze(0).to(dtype=torch.float32)
         solution = solution.expand(self.x.shape[0], -1)
-        
+
         # BASE_LENGTH is from global
-        pred = decoder.decode(solution, self.x, base_length=warm_up).view(-1).detach().cpu().numpy()
+        pred = (
+            decoder.decode(solution, self.x, base_length=warm_up)
+            .view(-1)
+            .detach()
+            .cpu()
+            .numpy()
+        )
 
         ob = self.y.view(-1).detach().cpu().numpy()[warm_up:]
-                
+
         return HydroErr.kge_2009(simulated_array=pred, observed_array=ob)
 
-# Hyperparameters of GA
-num_generations = 30
-num_parents_mating = 10
 
-sol_per_pop = 40
+# Hyperparameters of GA
 num_genes = 8
 
 init_range_low = -11
@@ -109,8 +149,8 @@ mutation_type = "random"
 mutation_probability = 0.25
 
 
-fn_cal = Objective_builder(x_cal,y_cal)
-fn_test = Objective_builder(x_test,y_test)
+fn_cal = Objective_builder(x_cal, y_cal)
+fn_test = Objective_builder(x_test, y_test)
 
 
 # Identifying optimal number of generations
@@ -120,40 +160,45 @@ progress_text = "Optimization in progress. Please wait."
 
 my_bar = st.progress(0, text=progress_text)
 
-def on_generation(instance):
-    
-    generations_completed = instance.generations_completed
-    
-    my_bar.progress(generations_completed/num_generations, text = progress_text)
-    
-ga_instance = pygad.GA(num_generations=num_generations,
-                    num_parents_mating=num_parents_mating,
-                    fitness_func=fn_cal.eval,
-                    sol_per_pop=sol_per_pop,
-                    num_genes=num_genes,
-                    init_range_low=init_range_low,
-                    init_range_high=init_range_high,
-                    parent_selection_type=parent_selection_type,
-                    crossover_type=crossover_type,
-                    mutation_type=mutation_type,
-                    mutation_probability = mutation_probability,
-                    on_generation=on_generation)
 
-if 'clicked' not in st.session_state:
+def on_generation(instance):
+    generations_completed = instance.generations_completed
+
+    my_bar.progress(generations_completed / num_generations, text=progress_text)
+
+
+ga_instance = pygad.GA(
+    num_generations=num_generations,
+    num_parents_mating=num_parents_mating,
+    fitness_func=fn_cal.eval,
+    sol_per_pop=sol_per_pop,
+    num_genes=num_genes,
+    init_range_low=init_range_low,
+    init_range_high=init_range_high,
+    parent_selection_type=parent_selection_type,
+    crossover_type=crossover_type,
+    mutation_type=mutation_type,
+    mutation_probability=mutation_probability,
+    on_generation=on_generation,
+)
+
+# run optimization
+if "clicked" not in st.session_state:
     st.session_state.clicked = False
+
 
 def click_button():
     st.session_state.clicked = True
 
-st.button('Run optimization', on_click=click_button)
+
+st.button("Run optimization", on_click=click_button)
 
 if st.session_state.clicked:
     ga_instance.run()
-    
+
     kge_cal = round(ga_instance.best_solution()[1], 3)
     kge_test = round(fn_test.eval(0, ga_instance.best_solution()[0], 0), 3)
 
-    f"Performance of the calibrated model instance: :red[**KGE={kge_cal}**], :red[**Test KGE={kge_test}**]."
-    
-    st.session_state.clicked = False
+    f"Performance of the optimal (i.e., calibrated) model instance: :red[**KGE={kge_cal}**], :red[**Test KGE={kge_test}**]."
 
+    st.session_state.clicked = False
